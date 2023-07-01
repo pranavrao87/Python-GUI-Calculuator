034a0995973fb941ba2a1a377993fdcc4bfd6071
Pranav Rao <108501622+pranavrao87@users.noreply.github.com>
2023-06-30 21:38:34 -0500

    Calculator

---
+++ Calculator.py
@@ -0,0 +1,33 @@
+import sys
+from functools import partial
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+WINDOW_SIZE = 235
+DISPLAY_HEIGHT = 35
+BUTTON_SIZE = 40
+ERROR_MSG = "ERROR"
+
+
+class PyCalcWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyCalc")
+        self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.generalLayout = QVBoxLayout()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+        self._createDisplay()
+        self._createButtons()
+
+    
